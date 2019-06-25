00f415d461d962273d9e5102b220bd47bfca3ca6
noob-wale <52056207+noob-wale@users.noreply.github.com>
2019-06-25 13:10:42 +0300

    Add files via upload

---
+++ age.py
@@ -0,0 +1,11 @@
+#import datetime to
+import datetime
+now = datetime.datetime.now()
+print(now.year, now.month, now.day)
+#request for the user's name
+user_name = input('Hi. Please enter your name:')
+#request for the user's age
+print('Hello, ' , user_name)
+user_age = int(input('Please enter your age: '))
+#print and calculate year they are turning 100
+print('You will turn 100 in the year,' , ((now.year-user_age+100)))
